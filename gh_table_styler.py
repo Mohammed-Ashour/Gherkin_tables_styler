660c7966f3b8a5b3ddd4a36c0c66dbceaffbd220
Mohammed_Ashour <m.aly.ashour@gmail.com>
2017-08-23 09:57:41 +0200

    some tweeking and error handling!

--- gh_table_styler.py
+++ gh_table_styler.py
@@ -15,24 +15,30 @@ def write_table(table, tab_index, out_file):
         out_file.writelines(tab_index*" " + line +"|"+"\n")
 
 
-in_file_name = "gh1.feature"
+in_file_name = input("input your file name : ")
 table = []
-with open(in_file_name) as gh_file:
-    out_file_name = "out" + in_file_name
-    out_file = open(out_file_name, "w")
-    TAB_SIZE = 0
-    for i in gh_file:
-        i_stripped = i.strip()
-        if len(i_stripped) > 0:
-            if i_stripped[0] == "|":
-                if not table : 
-                    TAB_SIZE = i.find("|")
-                table.append(list(map(methodcaller("strip"),  i_stripped[1:-1].split('|'))))  
-            else :
-                if table:
-                    write_table(table, TAB_SIZE, out_file)
-                    table = [] 
+try : 
+    with open(in_file_name) as gh_file:
+        out_file_name = "out" + in_file_name
+        out_file = open(out_file_name, "w")
+        TAB_SIZE = 0
+        for i in gh_file:
+            i_stripped = i.strip()
+            if len(i_stripped) > 0:
+                if i_stripped[0] == "|":
+                    if not table : 
+                        TAB_SIZE = i.find("|")
+                    table.append(list(map(methodcaller("strip"),  i_stripped[1:-1].split('|'))))  
+                else :
+                    if table:
+                        write_table(table, TAB_SIZE, out_file)
+                        table = [] 
 
-                out_file.writelines(i)
-        else :
-            out_file.writelines("\n")
+                    out_file.writelines(i)
+            else :
+                out_file.writelines("\n")
+except IOError:
+    print("there is IO error, please make sure that you entered a valid file!")
+    exit()
+
+print("Done styling {}, now you can check  {} for pretty tables!".format(in_file_name, out_file_name))
